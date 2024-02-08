70ccdfee0c20e8b9e8d3b8860acb744fd54a354c
Monterrosa <20220377@ricaldone.edu.sv>
2024-02-08 15:04:12 -0600

    version3

--- js.js
+++ js.js
@@ -6,3 +6,16 @@ function openTab(tabName) {
     }
     document.getElementById(tabName).style.display = "block";
 }
+
+// Función para agregar elementos a la lista de emprendedores
+function agregarEmprendedor(nombre, descripcion) {
+    var listaEmprendedores = document.getElementById("emprendedores-list");
+    var nuevoElemento = document.createElement("li");
+    nuevoElemento.textContent = nombre + " - " + descripcion;
+    listaEmprendedores.appendChild(nuevoElemento);
+}
+
+// Agregar emprendedores a la lista
+agregarEmprendedor("Jeff Bezos", "Fundador de Amazon...");
+agregarEmprendedor("Elon Musk", "Cofundador de PayPal, Tesla Motors, SpaceX y Neuralink...");
+// Agregar los demás emprendedores aquí..
